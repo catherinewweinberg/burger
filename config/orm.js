76edbf10b43a647941108023e279eccb3b47adc4
Catherine Weinberg <catherinewweinberg@gmail.com>
2019-07-28 17:11:07 -0400

    added in controllers in burgers_controller.js file, added all and create methods in orm.js file.

--- config/orm.js
+++ config/orm.js
@@ -1,4 +1,37 @@
 var connection = require("../config/connection");
+var orm = {
+  // all represents key of data containing function
+  all: function(tableInput, cb) {
+    var tableInput = "burgers";
+    var queryString = "SELECT * FROM " + tableInput + ";";
+    connection.query(queryString, function(err, res) {
+      if (err) {
+        throw err;
+      }
+      console.long(res);
+      cb(res);
+    });
+  },
+  create: function(table, cols, vals, cb) {
+    var queryString = "INSERT INTO " + table;
+
+    queryString += " (";
+    queryString += cols.toString();
+    queryString += ") ";
+    queryString += "VALUES (";
+    queryString += printQuestionMarks(vals.length);
+    queryString += ") ";
+
+    console.log(queryString);
+
+    connection.query(queryString, vals, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  }
+};
 
 // Methods that are required:
 // selectAll()
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,4 @@
 INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
 INSERT INTO burgers (burger_name) VALUES ('Bacon cheeseburger');
 INSERT INTO burgers (burger_name) VALUES ('Avacado Burger');
+
